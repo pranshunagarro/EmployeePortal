0a528aa218a35adfbe107f2bd62490926b99c57d
vikasnagarro <vikas.almal@nagarro.com>
2016-06-29 01:09:26 +0530

    Authenticate Services template code

---
+++ EmployeePortal/EmployeePortal.Core/WebServices/AuthenticateServices.cs
@@ -0,0 +1,17 @@
+﻿using EmployeePortal.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeePortal.Core.WebServices
+{
+    public class AuthenticateServices
+    {
+        public Employee Authentication (string userName, string password)
+        {
+            return null;
+        }
+    }
+}
